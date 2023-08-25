1b8e32666452d1a7e8bf95a3dadc99c429faeb05
Deyvison Silva <deyvisondasilva@gmail.com>
2023-08-25 17:51:18 -0300

    add - groupByDescricaoAndCountByDate funcionando

--- wbhealth/src/main/java/br/com/dbc/wbhealth/documentation/RelatorioLogControllerDoc.java
+++ wbhealth/src/main/java/br/com/dbc/wbhealth/documentation/RelatorioLogControllerDoc.java
@@ -1,11 +1,13 @@
 package br.com.dbc.wbhealth.documentation;
 
+import br.com.dbc.wbhealth.exceptions.DataInvalidaException;
 import br.com.dbc.wbhealth.model.dto.relatoriolog.RelatorioLogOutputDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,10 +16,23 @@ public interface RelatorioLogControllerDoc {
     @ApiResponses(
             value = {
                     @ApiResponse(responseCode = "200", description = "Retorna a quantidade de cada tipo de descrição de logs."),
+                    @ApiResponse(responseCode = "400", description = "Data inválida."),
                     @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
             }
     )
     @GetMapping("/group-and-count")
     public ResponseEntity<List<RelatorioLogOutputDTO>> groupByDescricaoAndCount();
+
+    @Operation(summary = "Conta a quantidade de logs, de um dia específico, agrupados pela descrição.", description = "Conta a quantidade de logs, de um dia específico, agrupados pela descrição dos logs.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna a quantidade de cada tipo de descrição de logs por data."),
+                    @ApiResponse(responseCode = "400", description = "Data inválida."),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
+            }
+    )
+    @GetMapping("/group-and-count-by-date")
+    public ResponseEntity<List<RelatorioLogOutputDTO>> groupByDescricaoAndCountByDate(@RequestParam String data) throws DataInvalidaException;
 }
