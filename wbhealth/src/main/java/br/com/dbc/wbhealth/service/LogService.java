87e902102ea26075c67913bf6d8590d306ea6eca
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-08-26 17:16:53 -0300

    Adicionado os testes de LogService

--- wbhealth/src/main/java/br/com/dbc/wbhealth/service/LogService.java
+++ wbhealth/src/main/java/br/com/dbc/wbhealth/service/LogService.java
@@ -6,19 +6,18 @@ import br.com.dbc.wbhealth.model.enumarator.Descricao;
 import br.com.dbc.wbhealth.repository.LogRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class LogService {
     private final LogRepository logRepository;
-    private final ObjectMapper objectMapper;
 
     public void create(Descricao descricao, Integer idUsuario) {
         LogEntity entity = new LogEntity();
@@ -31,7 +30,19 @@ public class LogService {
     public Page<LogOutputDTO> findAll(Integer page, Integer quantidadesLog) {
         Pageable pageable = PageRequest.of(page, quantidadesLog, Sort.Direction.DESC, "dataHora");
         Page<LogEntity> logPage = logRepository.findAll(pageable);
-        return logPage.map(log -> objectMapper.convertValue(log, LogOutputDTO.class));
 
+        List<LogOutputDTO> logOutputDTOs = new ArrayList<>();
+        for (LogEntity logEntity : logPage.getContent()) {
+            logOutputDTOs.add(convertToOutputDTO(logEntity));
+        }
+
+        return new PageImpl<>(logOutputDTOs, pageable, logPage.getTotalElements());
+    }
+
+    public LogOutputDTO convertToOutputDTO(LogEntity entity){
+        LogOutputDTO outputDTO = new LogOutputDTO();
+        BeanUtils.copyProperties(entity, outputDTO);
+        outputDTO.setDescricao(entity.getDescricao().name());
+        return outputDTO;
     }
 }
