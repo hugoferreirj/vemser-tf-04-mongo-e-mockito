8e009d5e467e3cb4b30016eed7c9cd42dabe93fc
Livia <liviafausto03@gmail.com>
2023-08-31 17:19:07 -0300

    Atributos laudo e receita do DTO + update UsuarioService

--- wbhealth/src/main/java/br/com/dbc/wbhealth/model/dto/atendimento/AtendimentoOutputDTO.java
+++ wbhealth/src/main/java/br/com/dbc/wbhealth/model/dto/atendimento/AtendimentoOutputDTO.java
@@ -1,18 +1,15 @@
 package br.com.dbc.wbhealth.model.dto.atendimento;
 
-import br.com.dbc.wbhealth.model.entity.AtendimentoEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.BeanUtils;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class AtendimentoOutputDTO {
 
     @Schema(description = "Id do atendimento.", example = "3")
@@ -41,9 +38,5 @@ public class AtendimentoOutputDTO {
 
     @Schema(description = "Valor do atendimento", example = "200")
     private Double valorDoAtendimento;
-
-    public AtendimentoOutputDTO(AtendimentoEntity entity) {
-        BeanUtils.copyProperties(entity, this);
-    }
 
 }
