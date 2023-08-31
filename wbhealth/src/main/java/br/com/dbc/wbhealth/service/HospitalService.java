6d85f7058ee29e188370a708842a55fbd56d809f
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-08-31 16:25:57 -0300

    atualizando o método convertAtendimentoToOutput

--- wbhealth/src/main/java/br/com/dbc/wbhealth/service/HospitalService.java
+++ wbhealth/src/main/java/br/com/dbc/wbhealth/service/HospitalService.java
@@ -2,7 +2,9 @@ package br.com.dbc.wbhealth.service;
 
 import br.com.dbc.wbhealth.exceptions.BancoDeDadosException;
 import br.com.dbc.wbhealth.exceptions.EntityNotFound;
+import br.com.dbc.wbhealth.model.dto.atendimento.AtendimentoMedicoDTO;
 import br.com.dbc.wbhealth.model.dto.atendimento.AtendimentoOutputDTO;
+import br.com.dbc.wbhealth.model.dto.atendimento.AtendimentoPacienteDTO;
 import br.com.dbc.wbhealth.model.dto.hospital.HospitalAtendimentoDTO;
 import br.com.dbc.wbhealth.model.dto.hospital.HospitalInputDTO;
 import br.com.dbc.wbhealth.model.dto.hospital.HospitalOutputDTO;
@@ -38,7 +40,7 @@ public class HospitalService {
     }
 
     public HospitalOutputDTO save(HospitalInputDTO hospitalInputDTO) throws BancoDeDadosException {
-        if(hospitalRepository.existsByCnpj(hospitalInputDTO.getCnpj())){
+        if (hospitalRepository.existsByCnpj(hospitalInputDTO.getCnpj())) {
             throw new BancoDeDadosException("CNPJ já cadastrado.");
         }
         HospitalEntity hospital = convertToEntity(hospitalInputDTO);
@@ -49,7 +51,7 @@ public class HospitalService {
 
     public HospitalOutputDTO update(Integer idHospital, HospitalInputDTO hospitalInputDTO) throws EntityNotFound, BancoDeDadosException {
         HospitalEntity hospital = getHospitalById(idHospital);
-        if(!Objects.equals(hospital.getCnpj(), hospitalInputDTO.getCnpj()) && hospitalRepository.existsByCnpj(hospitalInputDTO.getCnpj())){
+        if (!Objects.equals(hospital.getCnpj(), hospitalInputDTO.getCnpj()) && hospitalRepository.existsByCnpj(hospitalInputDTO.getCnpj())) {
             throw new BancoDeDadosException("CNPJ já cadastrado.");
         }
         hospital.setNome(hospitalInputDTO.getNome());
@@ -89,7 +91,7 @@ public class HospitalService {
         return hospitaisPaginados.map(this::convertToHospitalAtendimentosDTO);
     }
 
-    private HospitalAtendimentoDTO convertToHospitalAtendimentosDTO(HospitalEntity hospital){
+    private HospitalAtendimentoDTO convertToHospitalAtendimentosDTO(HospitalEntity hospital) {
         HospitalAtendimentoDTO hospitalAtendimentosOutput = new HospitalAtendimentoDTO();
 
         hospitalAtendimentosOutput.setIdHospital(hospital.getIdHospital());
@@ -104,15 +106,19 @@ public class HospitalService {
     }
 
     private AtendimentoOutputDTO convertAtendimentoToOutput(AtendimentoEntity atendimento) {
-        AtendimentoOutputDTO atendimentoOutputDTO = new AtendimentoOutputDTO();
-        atendimentoOutputDTO.setIdAtendimento(atendimento.getIdAtendimento());
+        AtendimentoOutputDTO atendimentoOutputDTO = objectMapper.convertValue(atendimento, AtendimentoOutputDTO.class);
+
         atendimentoOutputDTO.setIdHospital(atendimento.getHospitalEntity().getIdHospital());
-        atendimentoOutputDTO.setIdPaciente(atendimento.getPacienteEntity().getIdPaciente());
-        atendimentoOutputDTO.setIdMedico(atendimento.getMedicoEntity().getIdMedico());
-        atendimentoOutputDTO.setLaudo(atendimento.getLaudo());
-        atendimentoOutputDTO.setValorDoAtendimento(atendimento.getValorDoAtendimento());
-        atendimentoOutputDTO.setTipoDeAtendimento(atendimento.getTipoDeAtendimento().name());
-        atendimentoOutputDTO.setDataAtendimento(atendimento.getDataAtendimento());
+
+        AtendimentoMedicoDTO medico = new AtendimentoMedicoDTO();
+        medico.setIdMedico(atendimento.getMedicoEntity().getIdMedico());
+        medico.setNomeMedico(atendimento.getMedicoEntity().getPessoa().getNome());
+        atendimentoOutputDTO.setMedico(medico);
+
+        AtendimentoPacienteDTO paciente = new AtendimentoPacienteDTO();
+        paciente.setIdPaciente(atendimento.getPacienteEntity().getIdPaciente());
+        paciente.setNomePaciente(atendimento.getPacienteEntity().getPessoa().getNome());
+        atendimentoOutputDTO.setPaciente(paciente);
 
         return atendimentoOutputDTO;
     }
